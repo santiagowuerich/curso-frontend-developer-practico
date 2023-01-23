af268c5c8ba56ec299eb9d8351acf4f5dc11581d
santiagowuerich <santiagow142001@gmail.com>
2023-01-23 15:51:12 -0300

    Agregamos los productos detallados

--- main.js
+++ main.js
@@ -2,15 +2,34 @@ const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carritoMenu = document.querySelector('.product-detail')
+const carritoLogo =  document.querySelector('.navbar-shopping-cart')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+carritoLogo.addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu(){
    DesktopMenu.classList.toggle('inactive')
 };
 
 function toggleMobileMenu(){
+    const CarritoMenuOpen = carritoMenu.classList.contains('inactive');
+
+    if(!CarritoMenuOpen){
+        carritoMenu.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive')
+
  };
+
+function toggleCarritoMenu(){
+    const  MobileMenuCLose = mobileMenu.classList.contains('inactive');
+
+    if(!MobileMenuCLose){
+        mobileMenu.classList.add('inactive');
+    }
+    carritoMenu.classList.toggle('inactive');
+}
+
